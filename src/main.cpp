72f29be7911fbafeb3efef48c2b6bc4194ffa4b0
Fatbulat <fatbulat5@gmail.com>
2015-04-18 13:00:38 +0300

    Implement vector class template

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include "vector.h"
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello World" << endl;
+	float array[] = {1.6, 2, 3, 5};
+	KFU::Vector<float> v1(array, 4);
+	KFU::Vector<float> v2(v1);
+	v2[1]++;
+	v1 = v2;
+	cout << v1 << endl;
 	return 0;
 }
---
+++ src/vector.h
@@ -0,0 +1,85 @@
+#ifndef VECTOR_H_
+#define VECTOR_H_
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+namespace KFU
+{
+	enum class Orientation
+	{
+		vertical, horizontal
+	};
+
+	template <class type>
+	class Vector;
+
+	template <class type>
+	std::ostream& operator<<(std::ostream&, const Vector<type>&);
+
+
+	template <class type>
+	class Vector {
+		public:
+			Vector(type *, int, Orientation);
+			Vector(const Vector&);
+
+			int size() const;
+
+			type operator[](int) const;
+			type& operator[](int);
+			Vector& operator=(const Vector&);
+			Vector& operator+(const Vector&);
+		protected:
+			std::vector<type> values;
+			Orientation orient;
+			friend std::ostream& operator<<<type>(std::ostream&, const Vector&);
+	};
+
+	template <class type>
+	Vector<type>::Vector(type *array, int size, Orientation o = Orientation::horizontal): values(array, array + size), orient(o)
+	{
+	}
+
+	template <class type>
+	Vector<type>::Vector(const Vector& other): values(other.values), orient(other.orient)
+	{
+	}
+
+	template <class type>
+	int Vector<type>::size() const
+	{
+		return this->values.size();
+	}
+
+	template <class type>
+	type Vector<type>::operator[](int i) const
+	{
+		return values[i];
+	}
+
+	template <class type>
+	type& Vector<type>::operator[](int i)
+	{
+		return values[i];
+	}
+
+	template <class type>
+	Vector<type>& Vector<type>::operator=(const Vector& other)
+	{
+		values = other.values;
+		return *this;
+	}
+
+	template <class type>
+	std::ostream& operator<<(std::ostream& out, const Vector<type>& data)
+	{
+		char separator = (' ');
+		for (int i = 0; i < data.size(); i++)
+			out << data[i] << separator;
+			return out;
+	}
+}
+
+#endif
